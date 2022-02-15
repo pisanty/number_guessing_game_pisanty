4ed4d2e8b3d22aec4c6bac75e3619e3760b4c8dd
pisanty <pisantyshay@gmail.com>
2022-02-15 06:29:45 +0200

    second commit

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [guess, setGuess] = useState('');
+  const [count, setCount] = useState(1);
+  const [rndmnum, setRndmnum] = useState(0);
+
+  useEffect(() => gameOver(), [])
+
+  const gameOver = () => {
+    setRndmnum(Math.floor(Math.random() * 100) + 1);
+    setText('Guess a number between 1-100');
+    setCount(1);
+    setGuess('');
+  }
+
+  const feelinLucky = () => {
+    if (rndmnum === parseInt(guess)) {
+      Alert.alert(`Good job! You guessed the number in ${count} guesses!`);
+      gameOver();
+    }
+    else if (rndmnum < parseInt(guess)) {
+      setText(`Your guess ${guess} is too high!`);
+      setGuess('');
+    }
+    else {
+      setText(`Your guess ${guess} is too low!`);
+      setGuess('');
+    }
+
+    setCount(prevCount => prevCount + 1);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize: 30}}>{text}</Text>
+      <TextInput
+        keyboardType='numeric'
+        style={{fontSize: 25, width: 45, borderColor: 'black', backgroundColor: 'orange', borderWidth:2, margin: 2}}
+        onChangeText={guess => setGuess(guess)}
+        value={guess}/>
+      
+      <Button onPress={feelinLucky} title="Feelin' lucky?!" />
     </View>
   );
 }
